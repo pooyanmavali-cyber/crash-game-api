0f3d9f580c4a072a82df8b1b3218a7e1da1a4000
pooyanmavali-cyber <pooyanmavali@gmail.com>
2025-11-04 19:13:30 +0330

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(🎮 Server running on port ${PORT});
+  console.log('🎮 Server running on port ' + PORT);
 });
